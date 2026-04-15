cef820a9a12aea616cdce1da81f4e1bde0c19c82
zekar <zekar@horsefucker.org>
2026-04-14 22:28:03 -0600

    remove comment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Iniciando despliegue..."
-# git pull origin main # Se activará en la EC2
+git pull origin main # Se activará en la EC2
 docker compose down &&
   docker compose up --build -d &&
   echo "Despliegue finalizado con éxito" ||
