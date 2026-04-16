629960a186582b6f186314cb9659a79584d65f25
zekar <zekar@horsefucker.org>
2026-04-15 21:19:12 -0600

    remove comment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ DATE="date +%F/%R:%S"
 {
     cd "$(dirname "$0")/.." || exit 1
     echo "[$($DATE)]" "Iniciando despliegue..."
-    # git pull origin main # Se activará en la EC2
+    git pull origin main # Se activará en la EC2
     docker compose down &&
         docker compose up --build -d &&
         echo "[$($DATE)]" "Despliegue finalizado con éxito" ||
